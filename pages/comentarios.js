21c696be296009c492912441cc5796cace031874
Thiago Neves <thiago-antonio.neves@unesp.br>
2020-12-23 16:30:47 -0300

    Seing if the API is working on comments section

--- pages/comentarios.js
+++ pages/comentarios.js
@@ -11,27 +11,101 @@ class Coment extends Component{
             comenter_email: "",
             comenter_name: "",
             coment: "",
+            server_res: "",
         }
     }
 
-    inserir = () => {
+    componentDidMount(){
+        axios({
+            method: "GET",
+            url: "https://t-hub-api.herokuapp.com/coment/check",
+          }).then(function (response) {
+              if(response === "true")
+              {
+                this.setState({server_res :(<>
+                
+                <div className="container">
+                    <div class="jumbotron">
+                        <div className="container">
+                        </div>
+                        <h1 class="display-4">Here you can leave a comment to me!</h1>
+                        <hr class="my-4"/>
+                        <form>
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                            <label for="inputEmail4">Name</label>
+                            <input type="text" class="form-control" id="inputEmail4" placeholder="Name"
+                                onChange={(e)=>{
+                                    this.setState({
+                                        comenter_name: e.target.value,
+                                    })
+                                }}
+                            />
+                            </div>
+                            <div class="form-group col-md-6">
+                            <label for="inputPassword4">Email</label>
+                            <input type="email" class="form-control" id="inputPassword4" placeholder="Email"
+                                onChange={(e)=>{
+                                    this.setState({
+                                        comenter_email: e.target.value,
+                                    })
+                                }}
+                            />
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <label for="Comment">Comment</label>
+                            <textarea className="form-control" id="Comment" placeholder="Your comment goes here"
+                                onChange={(e)=>{
+                                    this.setState({
+                                        coment: e.target.value,
+                                    })
+                                }}
+                            ></textarea>
+                        </div>
+                        <br/>
+                        <button type="submit" class="btn btn-success" onClick={this.inserir}>Send</button>
+                        </form>
+                        </div>
+                    </div>
+                    
+                </>)}
+                )
+              }
+              else{
+                this.setState({
+                    server_res: (<>
+                        <div className="container">
+                            <div className="jumbotron">
+                                Unfortunately our API isn't working as it was supposed to! Sorry for the inconvenience.
+                            </div>
+                        </div>
+                    </>)
+                })
+              }
+            }).catch(function (response) {
+                this.setState({
+                    server_res: (<>
+                        <div className="container">
+                            <div className="jumbotron">
+                                Unfortunately our API isn't working as it was supposed to! Sorry for the inconvenience.
+                            </div>
+                        </div>
+                    </>)
+                })
+            });
+    }
 
-    axios.post('https://t-hub-api.herokuapp.com/coment/add',{
-        data: this.state
-    },{'content-type': 'application/json'}).then(function(response){
-        alert("Dados inseridos com sucesso");
-    }).catch(function(response){
-        alert("Erro na insercao dos dados")
-    })
-    // axios({
-    //         method: "POST",
-    //         url: "https://t-hub-api.herokuapp.com/coment/add",
-    //         data: this.state,
-    //       }).then(function (response) {
-    //           alert(response);
-    //         }).catch(function (response) {
-    //           alert(response);
-    //         });
+    inserir = () => {
+     axios({
+             method: "POST",
+             url: "https://t-hub-api.herokuapp.com/coment/add",
+             data: this.state,
+           }).then(function (response) {
+               alert(response);
+             }).catch(function (response) {
+               alert(response);
+             });
     }
 
     render(){
@@ -42,50 +116,7 @@ class Coment extends Component{
         <Navbar/>
         <br/>
         <center>
-        <div className="container">
-        <div class="jumbotron">
-            <div className="container">
-            </div>
-            <h1 class="display-4">Here you can leave a comment to me!</h1>
-            <hr class="my-4"/>
-            <form>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                <label for="inputEmail4">Name</label>
-                <input type="text" class="form-control" id="inputEmail4" placeholder="Name"
-                    onChange={(e)=>{
-                        this.setState({
-                            comenter_name: e.target.value,
-                        })
-                    }}
-                />
-                </div>
-                <div class="form-group col-md-6">
-                <label for="inputPassword4">Email</label>
-                <input type="email" class="form-control" id="inputPassword4" placeholder="Email"
-                    onChange={(e)=>{
-                        this.setState({
-                            comenter_email: e.target.value,
-                        })
-                    }}
-                />
-                </div>
-            </div>
-            <div className="form-row">
-                <label for="Comment">Comment</label>
-                <textarea className="form-control" id="Comment" placeholder="Your comment goes here"
-                    onChange={(e)=>{
-                        this.setState({
-                            coment: e.target.value,
-                        })
-                    }}
-                ></textarea>
-            </div>
-            <br/>
-            <button type="submit" class="btn btn-success" onClick={this.inserir}>Send</button>
-            </form>
-            </div>
-        </div>
+        {this.state.server_res}
         </center>
     </div>)
     }
